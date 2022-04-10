1943d4411e2adb988291e21752c8dce479f5c3cb
Angus <Traverser04@gmail.com>
2022-04-10 20:47:40 +0800

    Aad

---
+++ src/main/java/com/ispan/springbootdemo/dto/ShopcartDto.java
@@ -0,0 +1,11 @@
+package com.ispan.springbootdemo.dto;
+
+import java.io.Serializable;
+
+public class ShopcartDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
